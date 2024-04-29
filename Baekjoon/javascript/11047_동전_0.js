d086164f1d7cf26b76f651eaedc2567106cdf10e
xpecter <menistream@gmail.com>
2024-04-29 15:45:04 +0900

    chore : move file
    - javascript 폴더를 추가했습니다.
    - 파일을 각각 작성된 언어의 폴더로 옮겼습니다.

---
+++ Baekjoon/javascript/11047_동전_0.js
@@ -0,0 +1,26 @@
+// [백준11047](https://www.acmicpc.net/problem/10026)
+
+// 백준 입력
+// const inputs = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// IDE 입력
+const inputs = require('fs')
+  .readFileSync('./inputs.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+let [N, K] = inputs.shift().split(' ').map(Number);
+const coins = inputs.map(Number);
+let answer = 0;
+
+for (let i = coins.length - 1; i >= 0; i--) {
+  if (K / coins[i] >= 1) {
+    answer += Math.floor(K / coins[i]);
+    K %= coins[i];
+
+    if (K === 0) break;
+  }
+}
+
+console.log(answer);
